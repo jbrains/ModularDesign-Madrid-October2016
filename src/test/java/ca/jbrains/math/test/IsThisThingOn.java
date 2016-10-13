8cd2f96d26e2ab8ddf85ce7605aa775b679d98eb
J. B. Rainsberger <me@jbrains.ca>
2016-10-13 10:51:56 +0200

    The project module is now named for the actual project.

---
+++ src/test/java/ca/jbrains/math/test/IsThisThingOn.java
@@ -0,0 +1,12 @@
+package ca.jbrains.math.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsThisThingOn {
+    @Test
+    public void hello() throws Exception {
+
+        Assert.fail("Is this thing on?!");
+    }
+}
