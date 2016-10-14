6ce469b0d732055a74f25193dfad51f1a0c4ba01
J. B. Rainsberger <me@jbrains.ca>
2016-10-14 12:58:25 +0200

    The in-memory Catalog tests express their intent more directly.

--- src/test/java/ca/jbrains/pos/test/InMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/InMemoryCatalogTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class InMemoryCatalogTest {
@@ -12,9 +13,26 @@ public class InMemoryCatalogTest {
         final String barcode = "::barcode::";
         final Price price = Price.cents(700);
 
-        final InMemoryCatalog catalog
-                = new InMemoryCatalog(Collections.<String, Price> singletonMap(barcode, price));
+        final InMemoryCatalog catalog = createCatalogWith(barcode, price);
         Assert.assertEquals(price, catalog.findPrice(barcode));
+    }
+
+    @Test
+    public void productNotFound() throws Exception {
+        final InMemoryCatalog catalog = createCatalogWithout("::unknown::");
+        Assert.assertEquals(null, catalog.findPrice("::unknown::"));
+    }
+
+    private InMemoryCatalog createCatalogWith(String barcode, Price price) {
+        return new InMemoryCatalog(Collections.<String, Price>singletonMap(barcode, price));
+    }
+
+    private InMemoryCatalog createCatalogWithout(String barcodeToAvoid) {
+        return new InMemoryCatalog(new HashMap<String, Price>() {{
+            put("Not " + barcodeToAvoid, Price.cents(1));
+            put("Definitely not " + barcodeToAvoid, Price.cents(2));
+            put("Not " + barcodeToAvoid + " even a little bit", Price.cents(3));
+        }});
     }
 
     public static class InMemoryCatalog {
