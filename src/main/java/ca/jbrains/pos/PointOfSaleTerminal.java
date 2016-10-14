f07cfadff1999e0a445c5e6544d52cdeb2c5f173
J. B. Rainsberger <me@jbrains.ca>
2016-10-14 20:01:22 +0200

    We put everything together and it should just work, but it doesn't... yet.

--- src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
+++ src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
@@ -1,31 +1,34 @@
 package ca.jbrains.pos;
 
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class PointOfSaleTerminal {
     public static void main(String[] args) {
-        final SellOneItemController sellOneItemController = new SellOneItemController(
-                new InMemoryCatalog(new HashMap<String, Price>() {{
-                    put("8410055050011", Price.cents(179));
-                }}),
-                new Display() {
-                    @Override
-                    public void displayPrice(Price price) {
-                        System.out.printf("EUR %.2f", price.euro());
-                    }
 
-                    @Override
-                    public void displayProductNotFoundMessage(String barcodeNotFound) {
-                        System.out.printf("Product not found for %s", barcodeNotFound);
-                    }
+        final TextInputConsumerAndCommandInterpreter foo = new TextInputConsumerAndCommandInterpreter(
+                new SellOneItemController(
+                        new InMemoryCatalog(new HashMap<String, Price>() {{
+                            put("8410055050011", Price.cents(179));
+                        }}),
+                        new Display() {
+                            @Override
+                            public void displayPrice(Price price) {
+                                System.out.println(String.format("EUR %.2f", price.euro()));
+                            }
 
-                    @Override
-                    public void displayScannedEmptyBarcodeMessage() {
-                        System.out.printf("Scanning error: empty barcode");
-                    }
-                }
-        );
-        
-        sellOneItemController.onBarcode("8410055050011");
+                            @Override
+                            public void displayProductNotFoundMessage(String barcodeNotFound) {
+                                System.out.println(String.format("Product not found for %s", barcodeNotFound));
+                            }
+
+                            @Override
+                            public void displayScannedEmptyBarcodeMessage() {
+                                System.out.println(String.format("Scanning error: empty barcode"));
+                            }
+                        }
+                ));
+
+        foo.consume(new InputStreamReader(System.in));
     }
 }
