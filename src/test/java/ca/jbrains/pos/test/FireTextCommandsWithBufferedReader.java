46b06bfe5634bfa9ad7cbf461d797aecc5949a40
J. B. Rainsberger <me@jbrains.ca>
2016-10-15 18:16:29 +0200

    We now filter out unwanted whitespace from commands.

--- src/test/java/ca/jbrains/pos/test/FireTextCommandsWithBufferedReader.java
+++ src/test/java/ca/jbrains/pos/test/FireTextCommandsWithBufferedReader.java
@@ -5,15 +5,18 @@ import org.jmock.Sequence;
 import org.jmock.auto.Auto;
 import org.jmock.auto.Mock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class FireTextCommandsWithBufferedReader {
     @Rule
@@ -24,6 +27,24 @@ public class FireTextCommandsWithBufferedReader {
 
     @Auto
     private Sequence commandsSequence;
+    private FireTextCommands fireTextCommands;
+
+    // REFACTOR Move to a reusable library for test data.
+    public static List<String> nLinesLike(int n, String formatDescription) {
+        return IntStream.range(1, n).mapToObj(
+                (each) -> String.format(formatDescription, each)
+        ).collect(Collectors.toList());
+    }
+
+    // REFACTOR Move to generic text library
+    public static String unlines(List<String> lines) {
+        return String.join(System.lineSeparator(), lines);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        fireTextCommands = new FireTextCommands(textCommandListener);
+    }
 
     @Test
     public void onlyValidCommands() throws Exception {
@@ -39,21 +60,40 @@ public class FireTextCommandsWithBufferedReader {
             );
         }});
 
-        final FireTextCommands fireTextCommands = new FireTextCommands(textCommandListener);
-
         fireTextCommands.consumeText(new StringReader(
-                String.join(
-                        System.lineSeparator(),
-                        nLinesLike(5, "::valid command %d::")
-                )
+                unlines(nLinesLike(5, "::valid command %d::"))
         ));
     }
 
-    // REFACTOR Move to a reusable library for test data.
-    public static List<String> nLinesLike(int n, String formatDescription) {
-        return IntStream.range(1, n).mapToObj(
-                (each) -> String.format(formatDescription, each)
-        ).collect(Collectors.toList());
+    @Test
+    public void ignoreEmptyCommands() throws Exception {
+        context.checking(new Expectations() {{
+            never(textCommandListener);
+        }});
+
+        fireTextCommands.consumeText(new StringReader(
+                unlines(Arrays.asList(
+                        "",
+                        "\t",
+                        "\r",
+                        "\f",
+                        "\u000B",
+                        " ",
+                        " \t  \f  \u000B   "
+                ))
+        ));
+    }
+
+    @Test
+    public void trimWhitespaceFromCommands() throws Exception {
+        context.checking(new Expectations() {{
+            oneOf(textCommandListener).onCommand("::command\twith\finterior\u000Bspaces::");
+        }});
+
+        fireTextCommands.consumeText(new StringReader(
+                "  \t  \f  \u000B  \t ::command\twith\finterior\u000Bspaces::\t\t \f  \t   \u000B  \f \t  "
+        ));
+
     }
 
     public interface TextCommandListener {
@@ -68,9 +108,18 @@ public class FireTextCommandsWithBufferedReader {
         }
 
         public void consumeText(Reader textSource) {
-            new BufferedReader(textSource)
-                    .lines()
+            // Sigh. filter() and predicates and not().
+            // Read http://stackoverflow.com/a/30506585/253921
+            sanitizeLines(new BufferedReader(textSource)
+                    .lines())
                     .forEach(textCommandListener::onCommand);
+        }
+
+        // REFACTOR Make this a collaborator of the text consumer?
+        private Stream<String> sanitizeLines(Stream<String> lines) {
+            return lines
+                    .map(String::trim)
+                    .filter((line) -> !line.isEmpty());
         }
     }
 }
