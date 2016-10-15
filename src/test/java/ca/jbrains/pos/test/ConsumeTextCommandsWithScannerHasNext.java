0f5b09f3fa5fb8c75134a8a822cf668403da8757
J. B. Rainsberger <me@jbrains.ca>
2016-10-15 18:33:33 +0200

    We now document better the problem with using Scanner.hasNext() to consume text as commands.

--- src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsWithScannerHasNext.java
+++ src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsWithScannerHasNext.java
@@ -12,6 +12,10 @@ import java.io.BufferedReader;
 import java.io.StringReader;
 import java.util.Scanner;
 
+/*
+* This test remains here to document why we don't use
+* Scanner.hasNext() to implement "consume text as commands".
+ */
 public class ConsumeTextCommandsWithScannerHasNext {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
@@ -24,6 +28,7 @@ public class ConsumeTextCommandsWithScannerHasNext {
         context.checking(new Expectations() {{
             // According to the contract, there should be 3 commands,
             // each with empty text.
+            // This is the fault of Scanner.hasNext()
             exactly(0).of(textCommandListener).onCommand("");
         }});
 
@@ -40,6 +45,9 @@ public class ConsumeTextCommandsWithScannerHasNext {
 
         public void consume(StringReader stringReader) {
             final Scanner scanner = new Scanner(stringReader);
+            // Scanner.hasNext() causes empty lines not to be
+            // processed right away, but deferred until processing
+            // the next non-empty line.
             while (scanner.hasNext())
                 textCommandListener.onCommand(scanner.nextLine());
         }
