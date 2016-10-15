5e65efb6e70e150973932d5cbc3fd180cb4a8aad
J. B. Rainsberger <me@jbrains.ca>
2016-10-15 17:50:03 +0200

    We can now consume text as commands, as long as all the commands are valid.

--- src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsWithBufferedReader.java
+++ src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsWithBufferedReader.java
@@ -1,6 +1,8 @@
 package ca.jbrains.pos.test;
 
 import org.jmock.Expectations;
+import org.jmock.Sequence;
+import org.jmock.auto.Auto;
 import org.jmock.auto.Mock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
@@ -8,6 +10,9 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConsumeTextCommandsWithBufferedReader {
     @Rule
@@ -16,14 +21,38 @@ public class ConsumeTextCommandsWithBufferedReader {
     @Mock
     private TextCommandListener textCommandListener;
 
+    @Auto
+    private Sequence commandsSequence;
+
     @Test
-    public void onlyEmptyLines() throws Exception {
+    public void onlyValidCommands() throws Exception {
+        // Commands are "valid" if they have neither leading
+        // nor trailing whitespace. By implication, this means
+        // that they are not empty, either.
         context.checking(new Expectations() {{
-            exactly(3).of(textCommandListener).onCommand("");
+            nLinesLike(5, "::valid command %d::").stream().forEach(
+                    (eachCommand) -> {
+                        oneOf(textCommandListener).onCommand(eachCommand);
+                        inSequence(commandsSequence);
+                    }
+            );
         }});
 
-        new ConsumeTextInputAsLines(textCommandListener)
-                .consume(new StringReader("\n\n\n"));
+        final ConsumeTextInputAsLines consumeTextInputAsLines = new ConsumeTextInputAsLines(textCommandListener);
+
+        consumeTextInputAsLines.consume(new StringReader(
+                String.join(
+                        System.lineSeparator(),
+                        nLinesLike(5, "::valid command %d::")
+                )
+        ));
+    }
+
+    // REFACTOR Move to a reusable library for test data.
+    public static List<String> nLinesLike(int n, String formatDescription) {
+        return IntStream.range(1, n).mapToObj(
+                (each) -> String.format(formatDescription, each)
+        ).collect(Collectors.toList());
     }
 
     public interface TextCommandListener {
